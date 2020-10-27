9aecd6d1adff81b50a42758ddb59b36f6ae1a471
xuBlackDog <1325914621@qq.com>
2020-10-27 22:36:55 +0800

    '上传小图标'

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
   <div id="app">
     <router-view></router-view>
     <tar-bar>
-      <tar-bar-items path="/home" activeColor="blue">
-        <img class="tar_img" src="./assets/img/home.svg" slot="img" alt="">
-        <img class="tar_img" src="./assets/img/home_active.svg" slot="img-active" alt="">
+      <tar-bar-items path="/home">
+        <img class="tar_img" src="~/assets/img/tabbar/home.svg" slot="img" alt="">
+        <img class="tar_img" src="~/assets/img/tabbar/home_active.svg" slot="img-active" alt="">
         <div slot="text">主页</div>
       </tar-bar-items>
-      <tar-bar-items path="category" activeColor="black">
-         <img class="tar_img" src="./assets/img/category.svg" slot="img" alt="">
-         <img class="tar_img" src="./assets/img/category_active.svg" slot="img-active" alt="">
+      <tar-bar-items path="category">
+         <img class="tar_img" src="~/assets/img/tabbar/category.svg" slot="img" alt="">
+         <img class="tar_img" src="~/assets/img/tabbar/category_active.svg" slot="img-active" alt="">
          <div slot="text">消息</div>
       </tar-bar-items>
       <tar-bar-items path="cart">
-          <img class="tar_img" src="./assets/img/profile.svg" slot="img" alt="">
-          <img class="tar_img" src="./assets/img/profile_active.svg" slot="img-active" alt="">
+          <img class="tar_img" src="~/assets/img/tabbar/profile.svg" slot="img" alt="">
+          <img class="tar_img" src="~/assets/img/tabbar/profile_active.svg" slot="img-active" alt="">
           <div slot="text">我的</div>
       </tar-bar-items>
       <tar-bar-items path="profile">
-         <img class="tar_img" src="./assets/img/shopcart.svg" slot="img" alt="">
-         <img class="tar_img" src="./assets/img/shopcart_active.svg" slot="img-active" alt="">
+         <img class="tar_img" src="~/assets/img/tabbar/shopcart.svg" slot="img" alt="">
+         <img class="tar_img" src="~/assets/img/tabbar/shopcart_active.svg" slot="img-active" alt="">
           <div slot="text">购物车</div>
       </tar-bar-items>
     </tar-bar>
